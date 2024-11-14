b91a3214293412750e84b1bd22cb9e0881aa3c08
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-11-14 18:11:55 +0100

    Remove secrets/initfile.sql

--- secrets/initfile.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER IF NOT EXISTS root@localhost IDENTIFIED BY 'thisismyrootpassword';
-GRANT ALL ON *.* TO root@localhost WITH GRANT OPTION;
-SET PASSWORD FOR root@localhost = PASSWORD('thisismyrootpassword');
