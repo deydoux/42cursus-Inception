23a4b7bdaaaa0cbdde41d3652225ce4d2e3bbe8c
Dorian Eydoux <bonjour@dorian.cool>
2024-11-16 13:58:25 +0100

    Fix wordpress entrypoint

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -2,8 +2,8 @@
 
 if ! wp core is-installed; then
 	wp core download
-	wp config create --dbname=wordpress --dbuser=wordpress --dbpass="$(cat /run/secrets/password_db_wordpress.txt)" --dbhost=mariadb
-	wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_TITLE" --admin_user=root --admin_password="$(cat /run/secrets/password_wordpress_root.txt)" --admin_email="root@$DOMAIN_NAME"
+	wp config create --dbname=wordpress --dbuser=wordpress --dbhost=mariadb --prompt=dbpass < /run/secrets/password_db_wordpress.txt
+	wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_TITLE" --admin_user=root --admin_email="root@example.com" --prompt=admin_password < /run/secrets/password_wordpress_root.txt
 fi
 
 chown -R www:www .
