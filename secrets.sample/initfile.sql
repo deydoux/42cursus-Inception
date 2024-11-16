5d2690102da01dad6b8212bb4717c8845851ed36
Dorian Eydoux <bonjour@dorian.cool>
2024-11-16 13:39:19 +0100

    Update initfile.sql sample

--- secrets.sample/initfile.sql
+++ secrets.sample/initfile.sql
@@ -2,11 +2,11 @@ CREATE USER IF NOT EXISTS root@localhost IDENTIFIED BY "%ROOT_PASSWORD";
 SET PASSWORD FOR root@localhost = PASSWORD("%ROOT_PASSWORD");
 GRANT ALL PRIVILEGES ON *.* TO root@localhost WITH GRANT OPTION;
 
-CREATE USER 'healthcheck'@'localhost';
-GRANT USAGE ON *.* TO 'healthcheck'@'localhost';
+CREATE USER IF NOT EXISTS healthcheck@localhost;
+GRANT USAGE ON *.* TO healthcheck@localhost;
 
-CREATE USER IF NOT EXISTS wordpress@wordpress IDENTIFIED BY "%WORDPRESS_PASSWORD";
-SET PASSWORD FOR wordpress@wordpress = PASSWORD("%WORDPRESS_PASSWORD");
+CREATE USER IF NOT EXISTS wordpress@"%" IDENTIFIED BY "%WORDPRESS_PASSWORD";
+SET PASSWORD FOR wordpress@"%" = PASSWORD("%WORDPRESS_PASSWORD");
 CREATE DATABASE IF NOT EXISTS wordpress;
-GRANT ALL PRIVILEGES ON wordpress.* TO wordpress@wordpress;
+GRANT ALL PRIVILEGES ON wordpress.* TO wordpress@"%";
 FLUSH PRIVILEGES;
