c017e3f3edffaba5a3496772316ef9b6d5cba32b
Dorian Eydoux <bonjour@dorian.cool>
2024-11-16 17:02:52 +0100

    Add HTTPS to wp install URL

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -3,7 +3,7 @@
 if ! wp core is-installed; then
 	wp core download
 	wp config create --dbname=wordpress --dbuser=wordpress --dbhost=mariadb --prompt=dbpass < /run/secrets/password_db_wordpress.txt
-	wp core install --url="$DOMAIN_NAME" --title="$WORDPRESS_TITLE" --admin_user=root --admin_email="root@example.com" --prompt=admin_password < /run/secrets/password_wordpress_root.txt
+	wp core install --url="https://$DOMAIN_NAME" --title="$WORDPRESS_TITLE" --admin_user=root --admin_email="root@example.com" --prompt=admin_password < /run/secrets/password_wordpress_root.txt
 	wp user create deydoux "deydoux@student.42lyon.fr" --prompt=user_pass < /run/secrets/password_wordpress_deydoux.txt
 fi
 
