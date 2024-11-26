6cd1d7c1a010750dc202e27764333c6dbccc5c67
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-11-26 14:12:08 +0100

    Create bonus directory

--- srcs/requirements/adminer/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-curl -o index.php https://www.adminer.org/latest-mysql.php
-exec $@
--- srcs/requirements/adminer/healthcheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export REQUEST_METHOD="GET"
-export SCRIPT_FILENAME="/status"
-export SCRIPT_NAME="/status"
-
-cgi-fcgi -bind -connect localhost:9000
